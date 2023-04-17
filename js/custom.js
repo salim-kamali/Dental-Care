a7c397bf1cfbfcb2423dd8a83d665d70efc513ac
Salim Kamali <salim.kamali480@gmail.com>
2023-04-17 15:38:23 +0500

    first commmit

--- js/custom.js
+++ js/custom.js
@@ -33,33 +33,66 @@ $(document).ready(function () {
     });
 
 
+    $(".rev-slider").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        swipe: true,
+        infinite: false,
+        arrows: true,
+        autoplay: false,
+        draggable: true,
+        pauseOnHover: true,
+        appendArrows: '.rev-arrows',
+        responsive: [
+
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1.000,
+                    variableWidth: false,
+                    autoplay: false,
+                    adaptiveHeight: true,
+
+                }
+            }]
+    });
+
+
     if ($(window).width() < 990) {
 
         $(".cta-slider").slick({
-            slidesToShow:2,
-            slidesToScroll:1,
-            dots:false,
-            arrows:false,
-            infinite:true,
-            autoplay:true,
-            responsive:[{
-                breakpoint:767,
-                settings:{
-                    slidesToShow:1.00,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            infinite: true,
+            autoplay: true,
+            responsive: [{
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1.00,
                 }
             }]
         });
         $(".meet-the-team .row").slick({
-            slidesToShow:2,
-            slidesToScroll:1,
-            dots:false,
-            arrows:false,
-            infinite:true,
-            autoplay:true,
-            responsive:[{
-                breakpoint:767,
-                settings:{
-                    slidesToShow:1.00,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+            infinite: true,
+            autoplay: true,
+            responsive: [{
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1.00,
                 }
             }]
         });
